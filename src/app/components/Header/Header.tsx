abcb0c90eabdf1527884bde9e05b77f1b6180806
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-10-23 13:15:15 +0530

    designed header section for home page

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import styles from './Header.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
     return (
-        <div>
+        <div className={styles.headerSection}>
+            <Image 
+            src="https://www.onelot.ph/_next/image?url=%2Flogo.png&w=256&q=75"
+            alt='onelot-logo'
+            width={100}
+            height={35}
+            />
             
+            <ul className={styles.navLinks}>
+                <li><Link href="https://www.onelot.ph/">Home</Link></li>
+                <li><Link href="https://www.onelot.ph/products/dealer-inventory-loans">Products</Link></li>
+                <li><Link href="https://www.onelot.ph/about">About</Link></li>
+                <li><Link href="https://www.onelot.ph/contact">Contact</Link></li>
+            </ul>
+
+            <div className={styles.authButtons}>
+                <button type='button' className={styles.loginBtn}>Login</button>
+                <button type='button' className={styles.getStartedBtn}>Get Started</button>
+            </div>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import styles from './Page.module.scss';
+import IndexPage from './pages/index';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <IndexPage />
     </div>
   );
 }
