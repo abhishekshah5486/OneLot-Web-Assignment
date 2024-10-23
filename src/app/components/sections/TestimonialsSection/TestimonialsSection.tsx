6677443790c3a2d09a3ae3f559a0e95d59bb49f4
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-10-24 01:56:24 +0530

    Designed TestimonialsSection

--- src/app/components/sections/TestimonialsSection/TestimonialsSection.tsx
+++ src/app/components/sections/TestimonialsSection/TestimonialsSection.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './TestimonialsSection.module.scss';
+import TestimonialCard from '../../TestimonialCard/TestimonialCard';
 
 function TestimonialsSection() {
     return (
         <div className={styles.testimonialsSection}>
-        
+            <h2 className={styles.section_heading}>
+                Testimonials
+            </h2>
+            <div className={styles.testimonialsGrid}>
+                <TestimonialCard 
+                profile='https://www.onelot.ph/_next/image?url=%2FAvatar_1.png&w=256&q=75'
+                quote='The service of OneLot is top notch, they act fast and provide dealers with the funds they need, when they need it.'
+                author='Mark Vergel de Dios'
+                position='Owner'
+                organization='MVD Auto Works'
+                />
+            </div>
         </div>
     )
 }
