a4fe2b4d5d99b4d9d06d6e571a4f728cb0eb69a3
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-10-24 18:33:53 +0530

    Added an event listener for scroll effect

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+'use client';
+
+import React, {useEffect, useState} from 'react';
 import styles from './Header.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState<boolean>(false);
+    const handleScroll = () => 
+        {
+            if (window.scrollY > 0) setIsScrolled(true);
+            else setIsScrolled(false);
+        }
+
+    useEffect(() => 
+    {
+        window.addEventListener('scroll', handleScroll);
+        return () => 
+        {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    })
     return (
-        <div className={styles.headerSection}>
+        <div className={`${styles.headerSection} ${isScrolled ? styles.scrolled : ''}`}>
             <Image 
             src="https://www.onelot.ph/_next/image?url=%2Flogo.png&w=256&q=75"
             alt='onelot-logo'
